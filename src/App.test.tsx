aeaf6f306ca9d24d169ecc63392609b432ba06c3
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-07 11:21:57 -0300

    Remove boilerplate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app outer box', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appOuterBox = screen.getByTestId('app-outer-box');
+  expect(appOuterBox).toBeInTheDocument();
 });
