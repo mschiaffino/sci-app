617a1adfe320303e7821efbc982d4d62885e949e
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-08 11:59:49 -0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import { ThemeProvider } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 
-import TestSuiteEditor from './components/TestSuiteEditor';
+import { browserHistory } from './utils/history';
 import { theme } from './theme';
+import TestSuiteEditor from './components/TestSuiteEditor';
 
 function App() {
   return (
     <Box data-testid="app-outer-box">
       <ThemeProvider theme={theme}>
         <Box padding={3}>
-          <TestSuiteEditor />
+          <Router history={browserHistory}>
+            <Switch>
+              <Route path="/test-suite-editor">
+                <TestSuiteEditor />
+              </Route>
+              <Redirect from="*" to="/test-suite-editor"></Redirect>
+            </Switch>
+          </Router>
         </Box>
       </ThemeProvider>
     </Box>
