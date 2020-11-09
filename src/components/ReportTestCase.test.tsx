7ebbdd92138b5a75c35a998d2656141b1078a982
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-08 22:02:22 -0300

    Add report test case component

---
+++ src/components/ReportTestCase.test.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+import { InteractionSymbolMap, TestCase } from '../types';
+import ReportTestCase from './ReportTestCase';
+import userEvent from '@testing-library/user-event';
+
+const testCaseMock: TestCase = {
+  interactions: [
+    { symbol: 'O' },
+    { symbol: 'S' },
+    { symbol: 'Z' },
+    { symbol: 'Z' },
+    { symbol: 'C' },
+  ],
+};
+
+describe('ReportTestCase', () => {
+  let rendered: any;
+  let rerender: Function;
+  const onChangeMock = jest.fn();
+
+  beforeEach(() => {
+    rendered = render(
+      <ReportTestCase testCase={testCaseMock} onChange={onChangeMock} />
+    );
+    rerender = rendered.rerender;
+  });
+
+  test('should display interactions sequence', () => {
+    expect(screen.getByText('O.S.Z.Z.C')).toBeInTheDocument();
+  });
+
+  describe('test case interactions steps', () => {
+    test('should display one checkbox per interaction', () => {
+      const checkboxes = screen.getAllByRole('checkbox');
+
+      expect(checkboxes.length).toBe(testCaseMock.interactions.length);
+    });
+
+    test('should display the numbered interaction labels', () => {
+      testCaseMock.interactions.forEach((interaction, index) => {
+        expect(
+          screen.getByText(`${index}. ${interaction.symbol}`)
+        ).toBeInTheDocument();
+      });
+    });
+
+    test('should map interactions names when available', () => {
+      const symbolMap: InteractionSymbolMap = {
+        O: 'Open',
+        S: 'Select',
+        Z: 'Zoom',
+        C: 'Close',
+      };
+      rerender(
+        <ReportTestCase
+          testCase={testCaseMock}
+          symbolMap={symbolMap}
+          onChange={onChangeMock}
+        />
+      );
+
+      testCaseMock.interactions.forEach((interaction, index) => {
+        expect(
+          screen.getByText(`${index}. ${symbolMap[interaction.symbol]}`)
+        ).toBeInTheDocument();
+      });
+    });
+
+    test('should return updated test case on checkbox change', () => {
+      const checkboxToClickIndex = 2;
+      const checkbox = screen.getAllByRole('checkbox')[checkboxToClickIndex];
+
+      userEvent.click(checkbox);
+
+      const expectedUpdatedTestCase = { ...testCaseMock };
+      expectedUpdatedTestCase.interactions[checkboxToClickIndex].checked = true;
+      expect(onChangeMock).toHaveBeenCalledWith(expectedUpdatedTestCase);
+    });
+  });
+
+  describe('test case result toggle buttons', () => {
+    let failedButton: HTMLElement;
+    let passedButton: HTMLElement;
+
+    beforeEach(() => {
+      failedButton = screen.getByLabelText('failed');
+      passedButton = screen.getByLabelText('passed');
+    });
+
+    test('should display test case result toggle button group', () => {
+      expect(passedButton).toBeInTheDocument();
+      expect(failedButton).toBeInTheDocument();
+    });
+
+    test('should return updated test case with failed result', () => {
+      userEvent.click(failedButton);
+
+      const expectedUpdatedTestCase = { ...testCaseMock, passed: false };
+      expect(onChangeMock).toHaveBeenCalledWith(expectedUpdatedTestCase);
+    });
+
+    test('should return updated test case with passeed result', () => {
+      userEvent.click(passedButton);
+
+      const expectedUpdatedTestCase = { ...testCaseMock, passed: true };
+      expect(onChangeMock).toHaveBeenCalledWith(expectedUpdatedTestCase);
+    });
+  });
+
+  describe('comment text field', () => {
+    let commentTextField: HTMLElement;
+    const comment = 'There where some issues when zooming in';
+
+    beforeEach(() => {
+      commentTextField = screen.getByLabelText(/comment/i);
+    });
+
+    test('should display comment text field', () => {
+      expect(commentTextField).toBeInTheDocument();
+    });
+
+    test('should display comment value', () => {
+      const testCaseWithComment = { ...testCaseMock, comment };
+
+      rerender(
+        <ReportTestCase
+          testCase={testCaseWithComment}
+          onChange={onChangeMock}
+        />
+      );
+
+      expect(screen.getByText(comment)).toBeInTheDocument();
+    });
+
+    test('should return updated test case with comment', () => {
+      userEvent.type(commentTextField, comment);
+
+      const expectedUpdatedTestCase = { ...testCaseMock, comment };
+      expect(onChangeMock).toHaveBeenCalledWith(expectedUpdatedTestCase);
+    });
+  });
+});
---
+++ src/components/ReportTestCase.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Checkbox from '@material-ui/core/Checkbox';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import ToggleButton from '@material-ui/lab/ToggleButton/ToggleButton';
+import TextField from '@material-ui/core/TextField';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+
+import { InteractionSymbolMap, TestCase } from '../types';
+
+interface Props {
+  testCase: TestCase;
+  symbolMap?: InteractionSymbolMap;
+  onChange: (testCase: TestCase) => void;
+}
+
+export default function ReportTestCase({
+  testCase,
+  symbolMap,
+  onChange,
+}: Props) {
+  const testCaseName = testCase.interactions.map((i) => i.symbol).join('.');
+
+  const handleInteractionCheckChange = (
+    event: any,
+    index: number,
+    checked: boolean
+  ) => {
+    const updatedTestCase: TestCase = { ...testCase };
+    updatedTestCase.interactions[index].checked = checked;
+    onChange(updatedTestCase);
+  };
+
+  const handleCommentChange = (event: any) => {
+    const updatedTestCase: TestCase = {
+      ...testCase,
+      comment: event.target.value,
+    };
+    onChange(updatedTestCase);
+  };
+
+  const handleResultChange = (
+    event: React.MouseEvent<HTMLElement>,
+    passed: boolean
+  ) => {
+    const updatedTestCase: TestCase = { ...testCase, passed };
+    onChange(updatedTestCase);
+  };
+
+  return (
+    <Box>
+      <Typography variant="h6">{testCaseName}</Typography>
+      <Box display="flex" flexDirection="column" justifyContent="flex-start">
+        {testCase.interactions.map((interaction, index) => (
+          <Box
+            display="flex"
+            alignItems="center"
+            key={interaction.symbol + index}
+          >
+            <Checkbox
+              checked={interaction.checked}
+              onChange={(event, checked) =>
+                handleInteractionCheckChange(event, index, checked)
+              }
+            ></Checkbox>
+            <Typography>
+              {interactionText(index, interaction.symbol, symbolMap)}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+      <TextField
+        label="Comment"
+        multiline
+        fullWidth={true}
+        value={testCase.comment}
+        inputProps={{ 'aria-label': 'comment' }}
+        onChange={handleCommentChange}
+      />
+      <Box marginTop={2} display="flex" alignItems="center">
+        <Typography variant="subtitle1">Result</Typography>
+        <Box marginLeft={1}>
+          <ToggleButtonGroup
+            value={testCase.passed}
+            exclusive
+            onChange={handleResultChange}
+          >
+            <ToggleButton value={true} aria-label="passed">
+              <CheckIcon htmlColor={green[800]} />
+            </ToggleButton>
+            <ToggleButton value={false} aria-label="failed">
+              <CloseIcon color="error" />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+const interactionText = (
+  index: number,
+  symbol: string,
+  symbolMap?: InteractionSymbolMap
+) => {
+  let name = symbol;
+  if (symbolMap && symbolMap[symbol]) {
+    name = symbolMap[symbol];
+  }
+  return `${index}. ${name}`;
+};
