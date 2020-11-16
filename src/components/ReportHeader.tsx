58c968eebdf69e0322704b619463438cf3a1f152
Martin Schiaffino <schiaffinomartin@gmail.com>
2020-11-16 11:33:17 -0300

    Add pdf generation button

--- src/components/ReportHeader.tsx
+++ src/components/ReportHeader.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 
 import { Report } from '../types';
 
@@ -10,8 +11,32 @@ interface Props {
 }
 
 export default function ReportHeeader({ report }: Props) {
+  const [visiblePdfIcon, setVisiblePdfIcon] = useState(true);
+
+  const onPdfIconClick = () => {
+    setVisiblePdfIcon(false);
+    setTimeout(() => {
+      window.print();
+    }, 0);
+    setTimeout(() => {
+      setVisiblePdfIcon(true);
+    }, 1000);
+  };
+
   return (
     <Box>
+      <Box display="flex" alignItems="center" marginBottom={1}>
+        <Typography variant="h3">Report</Typography>
+        <Box marginLeft={3}>
+          {visiblePdfIcon && (
+            <PictureAsPdfIcon
+              onClick={onPdfIconClick}
+              color="primary"
+              style={{ cursor: 'pointer' }}
+            />
+          )}
+        </Box>
+      </Box>
       <Box display="flex" alignItems="center" flexWrap="wrap" marginBottom={2}>
         <Box width={200}>
           <Typography variant="h5">SCI:&nbsp;</Typography>
